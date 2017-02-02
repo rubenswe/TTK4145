a848cfbaeb23e6b527f288e026c237f7e4464a64
Unknown <rubensw@stud.ntnu.no>
2017-02-02 12:22:30 +0100

    Updated to Python 3

--- Ex03/tcp_client.py
+++ Ex03/tcp_client.py
@@ -3,13 +3,15 @@ import socket
 HOST = "127.0.0.1"
 PORT = 20012
 
+
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     
     while True:
         print("Sending...")
-        s.sendall("Hello, world!")
+        msg = input("Message: ")
+        s.sendall(msg.encode("utf-8"))
         print("Receiving...")
         data = s.recv(1024)
         print("Received", repr(data))
