58c0e87891fd26bed191164d21ee56f54f602e93
Unknown <rubensw@stud.ntnu.no>
2017-03-11 18:07:04 +0100

    Working on request manager

--- Project/elevator/elevator/request_manager.py
+++ Project/elevator/elevator/request_manager.py
@@ -4,7 +4,6 @@ import process_pairs
 import transaction
 import network
 import core
-from elevator import user_interface
 
 
 class RequestManagerState(object):
@@ -23,21 +22,24 @@ class RequestManager(process_pairs.PrimaryBackupSwitchable):
     Receives user requests from the interface++
     """
 
-    def __init__(self, config):
+    def __init__(self, config, _network):
         assert isinstance(config, core.Configuration)
         # assert isinstance(transaction_manager, transaction.TransactionManager)
-        # assert isinstance(_network, network.Network)
+        assert isinstance(_network, network.Network)
 
-        # transaction.ResourceManager.__init__(self, transaction_manager)
+        self.network = _network
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        _network.add_packet_handler("elev_request_add", self.receive_request)
 
-    def init(self, user_interface):
-        pass
+        self.__floors = [FloorPendingRequest()] * 4
+        # self.__floors[0].internal = True
 
     def start(self):
         """
          Starts working from the current state
         """
-        pass
+
+        print("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
 
     def export_state(self):
         """
@@ -77,4 +79,32 @@ class RequestManager(process_pairs.PrimaryBackupSwitchable):
         Adds a new request from users.
         """
 
-        logging.debug("An request has been added! Target floor: {}".format(floor))
+        logging.debug("An request has been added! Target floor: {}".format(floor+1))
+        self.__floors[floor].internal = True
+
+
+    def receive_request(self, addr, data):
+        """
+        Receives an request from the Floor User Interface
+        """
+
+        logging.debug("An packet has been received!")
+
+        floor = data["floor"]
+        direction = data["direction"]
+
+        if direction == core.Direction.Up:
+            self.__floors[floor].up = True
+        elif direction == core.Direction.Down:
+            self.__floors[floor].down = True
+
+
+class FloorPendingRequest(object):
+    """
+    Data structure of the requests
+    """
+
+    def __init__(self):
+        self.up = False
+        self.down = False
+        self.internal = False
